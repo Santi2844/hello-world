b4c6a1d6845b4089ce6747a4f309708be627916a
Santi2844 <a01798818@tec.mx>
2025-11-11 06:36:31 -0600

    Add files via upload

---
+++ problema 2/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n1, n2, suma;
+
+    n1 = 8;
+    n2 = 5;
+
+    suma = n1 + n2;
+
+    cout << "La suma de " << n1 << " + " << n2 << " es: " << suma << endl;
+
+    return 0;
+}
